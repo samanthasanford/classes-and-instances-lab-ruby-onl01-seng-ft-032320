d63e6b49133a220d37aaea180c83746b8f81342d
Samantha Sanford <samanthawebdev@gmail.com>
2020-03-31 21:16:34 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+end
+
+fido = Dog.new
+fido
+
+snoopy = Dog.new
+snoopy
+
+lassie = Dog.new
+lassie
+
+class Person
+end
+
+adele_goldberg = Person.new
+adele_goldberg
+
+alan_kay = Person.new
+alan_kay
